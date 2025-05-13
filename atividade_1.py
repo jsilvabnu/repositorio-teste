21187ec6b74be4cf4aaf89d21d167c6079f21792
jsilvabnu <jsilvabnu@gmail.com>
2025-05-12 21:54:01 -0300

    logica inicial

---
+++ atividade_1.py
@@ -0,0 +1,72 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+api_key = os.getenv("API_KEY_NOTICIAS")
+
+if not api_key:
+    raise ValueError("API Key não encontrada nas variáveis de ambiente")
+
+url = "https://newsapi.org/v2/everything"
+
+temas = []
+
+def menu():
+
+    print("\nMENU")
+    print("0. Sair")
+    print("1. Consultar noticias")
+
+    opcao = input("Digite a opção do menu: ")
+    return opcao
+
+def buscar_noticias():
+    
+    tema = input("Digite o tema que deseja pesquisar: ")
+    temas.append(tema)
+    headers = {
+    'X-Api-Key': api_key
+    }
+
+    params = {
+        'q': {tema},
+        'language': "pt"
+    }
+    resposta = requests.get(url=url, headers=headers, params=params)
+    print(resposta.status_code)
+
+    resposta_json = resposta.json()
+
+
+    for artigo in resposta_json["articles"][:quantidade]:
+        print("\n")
+        print("Título: ", artigo["title"])
+        print("Descrição: ", artigo["description"])
+        print("Fonte: ", artigo["url"])
+        print("Autor: ", artigo["author"])
+
+while True:
+
+    opcao = menu()
+    if opcao == "0":
+        print("Você pesquisou sobre:", temas)
+        break
+    elif opcao == "1":
+        try:
+            quantidade = int(input("Digite a quantidade de noticia que deseja consultar (máximo de 5):"))
+        except ValueError:
+            print("Erro de valor. Tente novamente e digite um número válido.")
+            continue
+
+        if 5 >= quantidade > 0:
+            buscar_noticias()
+
+        else:
+            print("Quantidade inválida. Tente novamente.")
+    else:
+        print("Opção inválida. Tente novamente.")
+
+
+    
